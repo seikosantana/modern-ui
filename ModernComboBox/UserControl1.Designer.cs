136771f95afe9d423c23a1bb01e21d538df522e4
Seiko Santana <santanaaseiko@gmail.com>
2020-04-04 23:29:14 +0700

    Added new projects for more controls

--- ModernComboBox/UserControl1.Designer.cs
+++ ModernComboBox/UserControl1.Designer.cs
@@ -28,9 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(413, 30);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
